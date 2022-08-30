94c0b75ec2dc727fb495d8afa67a200b04a90f92
trasea986 <trasea986@hotmail.com>
2022-08-30 10:26:10 -0500

    added additional variables for radish

--- resistance_script_env_prep.R
+++ resistance_script_env_prep.R
@@ -19,7 +19,7 @@ setwd("../TerrainWorks_Dung_Elwha_Corrected/TerrainWorks_Dung_Elwha_Corrected/")
 tworks <- read_sf(dsn = ".", layer = "Terrainworks_Dung_Elwha")
 
 #from Amy is a template file for cropping both layers and set crs
-setwd("../env_data/Elwha_streams/")
+setwd("../../../env_data/Elwha_streams/")
 elwha <- read_sf(dsn = ".", layer = "Elwha_streams")
 
 #note different crs
@@ -57,12 +57,12 @@ norwest_crop <- st_crop(norwest_repro, elwha)
 tworks_crop <- st_crop(tworks_repro, elwha)
 
 #check cropped norwest
-# ggplot() + 
-#   geom_sf(data = tworks_crop, size = 1, color = "blue") + 
-#   geom_sf(data = norwest_crop, size = 1, color = "black") +
-#   ggtitle("AOI Boundary Plot") + 
-#   coord_sf() +
-#   theme_bw()
+ggplot() +
+  geom_sf(data = tworks_crop, size = 1, color = "blue") +
+  geom_sf(data = norwest_crop, size = 1, color = "black") +
+  ggtitle("AOI Boundary Plot") +
+  coord_sf() +
+  theme_bw()
 
 
 ####next is create a raster from each of the values we are interested in
@@ -83,12 +83,12 @@ aug_st_temp <- terra::rasterize(norwest_crop, ra, field = "S2_02_11")
 aug_st_temp_df <- as.data.frame(aug_st_temp, xy = TRUE)
 
 #plot to check
-# ggplot() + 
-#   geom_raster(data = aug_st_temp_df, aes(x = x, y = y, fill = S2_02_11)) +
-#   scale_fill_continuous(type = "viridis", na.value = "white") +
-#   ggtitle("S2_02_11") + 
-#   coord_sf() +
-#   theme_bw()
+ggplot() +
+  geom_raster(data = aug_st_temp_df, aes(x = x, y = y, fill = S2_02_11)) +
+  scale_fill_continuous(type = "viridis", na.value = "white") +
+  ggtitle("S2_02_11") +
+  coord_sf() +
+  theme_bw()
 
 #vector
 tworks_crop <- vect(tworks_crop)
@@ -100,22 +100,46 @@ tworks_flow <- terra::rasterize(tworks_crop, ra, field = "FlowVel")
 tworks_flow_df <- as.data.frame(tworks_flow, xy = TRUE)
 
 #plot
-# ggplot() + 
-#   geom_raster(data = tworks_flow_df, aes(x = x, y = y, fill = FlowVel)) +
-#   scale_fill_continuous(type = "viridis", na.value = "white") +
-#   ggtitle("S2_02_11") + 
-#   coord_sf() +
-#   theme_bw()
+ggplot() +
+  geom_raster(data = tworks_flow_df, aes(x = x, y = y, fill = FlowVel)) +
+  scale_fill_continuous(type = "viridis", na.value = "white") +
+  ggtitle("S2_02_11") +
+  coord_sf() +
+  theme_bw()
+
+
+
+#this all works, so now just need to pull in other pieces
+#and then rasterize
+tworks_flow <- terra::rasterize(tworks_crop, ra, field = "FlowVel")
+tworks_flow_df <- as.data.frame(tworks_flow, xy = TRUE)
+tworks_bfq <- terra::rasterize(tworks_crop, ra, field = "BFQ")
+tworks_bfq_df <- as.data.frame(tworks_bfq, xy = TRUE)
+aug_st_temp <- terra::rasterize(norwest_crop, ra, field = "S2_02_11")
+aug_st_temp_df <- as.data.frame(aug_st_temp, xy = TRUE)
+slope <- terra::rasterize(norwest_crop, ra, field = "SLOPE")
+slope_df <- as.data.frame(slope, xy = TRUE)
+canopy <- terra::rasterize(norwest_crop, ra, field = "CANOPY")
+canopy_df <- as.data.frame(canopy, xy = TRUE)
+cumdrainag <- terra::rasterize(norwest_crop, ra, field = "CUMDRAINAG")
+cumdrainag_df <- as.data.frame(cumdrainag, xy = TRUE)
 
 #convert to raster file type
 tworks_flow <- raster::raster(tworks_flow)
+tworks_bfq <- raster::raster(tworks_bfq)
 aug_st_temp <- raster::raster(aug_st_temp)
+slope <- raster::raster(slope)
+canopy <- raster::raster(canopy)
+cumdrainag <- raster::raster(cumdrainag)
 
-#one other step is to remove -9999 and make NA
+#one other step is to remove -9999 and make NA for all NW temp data
 aug_st_temp <- raster::reclassify(aug_st_temp, cbind(-Inf, 0, NA), right=FALSE)
+slope <- raster::reclassify(slope, cbind(-Inf, 0, NA), right=FALSE)
+canopy <- raster::reclassify(canopy, cbind(-Inf, 0, NA), right=FALSE)
+cumdrainag <- raster::reclassify(cumdrainag, cbind(-Inf, 0, NA), right=FALSE)
 
 #stack raster for radish
-env_stack <- raster::stack(tworks_flow, aug_st_temp)
+env_stack <- raster::stack(tworks_flow, tworks_bfq, aug_st_temp, slope, canopy, cumdrainag)
 
 #dataframe from the stack for checking final alignment with plot
 df1 <- as.data.frame(env_stack$FlowVel, xy = TRUE)
